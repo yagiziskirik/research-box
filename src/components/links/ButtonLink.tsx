b9cedca330876d66d494fa3f14864339d7aace20
Yağız Işkırık <y_iskirik_10@hotmail.com>
2023-06-04 06:05:23 +0300

    :racehorse: Improve SEO performance

--- src/components/links/ButtonLink.tsx
+++ src/components/links/ButtonLink.tsx
@@ -71,14 +71,14 @@ const ButtonLink = React.forwardRef<HTMLAnchorElement, ButtonLinkProps>(
               'border-primary-500 border',
               'hover:bg-primary-50 active:bg-primary-100 disabled:bg-primary-100',
               isDarkBg &&
-              'hover:bg-gray-900 active:bg-gray-800 disabled:bg-gray-800',
+                'hover:bg-gray-900 active:bg-gray-800 disabled:bg-gray-800',
             ],
             variant === 'ghost' && [
               'text-primary-500',
               'shadow-none',
               'hover:bg-primary-50 active:bg-primary-100 disabled:bg-primary-100',
               isDarkBg &&
-              'hover:bg-gray-900 active:bg-gray-800 disabled:bg-gray-800',
+                'hover:bg-gray-900 active:bg-gray-800 disabled:bg-gray-800',
             ],
             variant === 'light' && [
               'bg-white text-gray-700',
